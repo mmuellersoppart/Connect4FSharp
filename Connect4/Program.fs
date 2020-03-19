e967a0a61443e68d128ade8dd2d78a61ea2903a2
Marlon Mueller-Soppart <mmuellersoppart@gmail.com>
2020-03-19 09:42:02 -0500

    empty board initializes and prints

---
+++ Connect4/Program.fs
@@ -0,0 +1,44 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let makeBoard numRows numCols =
+     Array2D.init<char> numRows numCols (fun row col -> 'x')
+
+//https://en.wikibooks.org/wiki/F_Sharp_Programming/Arrays
+let printBoard board =
+    let maxY = (Array2D.length1 board) - 1
+    let maxX = (Array2D.length2 board) - 1
+    
+    for row in 0 .. maxY do
+        for col in 0 .. maxX do
+            printf "%A" board.[row, col] 
+        Console.WriteLine()
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Connect 4. Prepare to have fun."
+    
+    let NumRows = 6
+    let NumCols = 7
+    
+    let board = makeBoard NumRows NumCols
+    
+    printBoard board
+    
+    let rec _playGame board playerTurn =
+        match playerTurn with
+        | 'X' -> playerXMakeMove()
+        | 'Y' -> playerYMakeMove()
+        
+        let gameOver = isGameOver board
+        match gameOver with
+        | true -> printfn "Game Over. We are done here."
+        | false -> ()
+        
+        printBoard board
+        
+        
+    
+    0 // return an integer exit code
